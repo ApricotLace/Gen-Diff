c83a4ffa039f9ad9954200be1d7f2091a798d013
apricotlace <asuracoi@gmail.com>
2018-11-20 02:18:14 +0300

    Test's refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,19 +2,18 @@ import { readFileSync } from 'fs';
 import genDiff from '../src';
 
 const getPathToFixture = fileName => `__tests__/__fixtures__/${fileName}`;
+const expected = readFileSync(getPathToFixture('expected'), 'utf8');
 
 describe('Compare flat configs', () => {
   it('#JSON', () => {
     const conf1 = getPathToFixture('before.json');
     const conf2 = getPathToFixture('after.json');
-    const expected = readFileSync(getPathToFixture('expected'), 'utf8');
     expect(genDiff(conf1, conf2)).toBe(expected);
   });
 
   it('#YAML', () => {
     const conf1 = getPathToFixture('before.yaml');
     const conf2 = getPathToFixture('after.yaml');
-    const expected = readFileSync(getPathToFixture('expected'), 'utf8');
     expect(genDiff(conf1, conf2)).toBe(expected);
   });
 });
