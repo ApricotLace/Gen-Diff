78ed1f376da2be3f06e11a674d9fef6556118737
apricotlace <asuracoi@gmail.com>
2018-11-20 02:14:05 +0300

    Add new test, also add new fixtures (for YAML ext.)

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,20 @@
-import fs from 'fs';
+import { readFileSync } from 'fs';
 import genDiff from '../src';
 
 const getPathToFixture = fileName => `__tests__/__fixtures__/${fileName}`;
 
-describe('genDiff', () => {
-  test('#compare flat configs', () => {
+describe('Compare flat configs', () => {
+  it('#JSON', () => {
     const conf1 = getPathToFixture('before.json');
     const conf2 = getPathToFixture('after.json');
-    const expected = fs.readFileSync(getPathToFixture('expected'), 'utf8');
+    const expected = readFileSync(getPathToFixture('expected'), 'utf8');
+    expect(genDiff(conf1, conf2)).toBe(expected);
+  });
+
+  it('#YAML', () => {
+    const conf1 = getPathToFixture('before.yaml');
+    const conf2 = getPathToFixture('after.yaml');
+    const expected = readFileSync(getPathToFixture('expected'), 'utf8');
     expect(genDiff(conf1, conf2)).toBe(expected);
   });
 });
