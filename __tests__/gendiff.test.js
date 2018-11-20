5a43e1fe69c45eda50c85c6e3bd0006ec8819cf8
apricotlace <asuracoi@gmail.com>
2018-11-20 17:28:12 +0300

    Add new test, add new fixtures (for .ini files)

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,14 +2,20 @@ import { readFileSync } from 'fs';
 import genDiff from '../src';
 
 const getPathToFixture = fileName => `__tests__/__fixtures__/${fileName}`;
-const expected = readFileSync(getPathToFixture('expected'), 'utf8');
 
 describe('Compare flat configs', () => {
   it('#JSON', () => {
-    expect(genDiff(getPathToFixture('before.json'), getPathToFixture('after.json'))).toBe(expected);
+    expect(genDiff(getPathToFixture('before.json'), getPathToFixture('after.json')))
+      .toBe(readFileSync(getPathToFixture('expected'), 'utf8'));
   });
 
   it('#YAML', () => {
-    expect(genDiff(getPathToFixture('before.yaml'), getPathToFixture('after.yaml'))).toBe(expected);
+    expect(genDiff(getPathToFixture('before.yaml'), getPathToFixture('after.yaml')))
+      .toBe(readFileSync(getPathToFixture('expected'), 'utf8'));
+  });
+
+  it('#INI', () => {
+    expect(genDiff(getPathToFixture('before.ini'), getPathToFixture('after.ini')))
+      .toBe(readFileSync(getPathToFixture('expected'), 'utf8'));
   });
 });
