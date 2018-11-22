76431e53765c7f9a698961dc6ceb39b7c785b550
apricotlace <asuracoi@gmail.com>
2018-11-22 03:25:23 +0300

    Add new tests (For nested .json / .yaml / .ini formats), add new fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,18 +4,39 @@ import genDiff from '../src';
 const getPathToFixture = fileName => `__tests__/__fixtures__/${fileName}`;
 
 describe('Compare flat configs', () => {
+  const expected = readFileSync(getPathToFixture('expected'), 'utf8');
+
   it('#JSON', () => {
     expect(genDiff(getPathToFixture('before.json'), getPathToFixture('after.json')))
-      .toBe(readFileSync(getPathToFixture('expected'), 'utf8'));
+      .toBe(expected);
   });
 
   it('#YAML', () => {
     expect(genDiff(getPathToFixture('before.yaml'), getPathToFixture('after.yaml')))
-      .toBe(readFileSync(getPathToFixture('expected'), 'utf8'));
+      .toBe(expected);
   });
 
   it('#INI', () => {
     expect(genDiff(getPathToFixture('before.ini'), getPathToFixture('after.ini')))
-      .toBe(readFileSync(getPathToFixture('expected'), 'utf8'));
+      .toBe(expected);
+  });
+});
+
+describe('Compare nested configs', () => {
+  const expected = readFileSync(getPathToFixture('nestedExpected'), 'utf8');
+
+  it('#JSON', () => {
+    expect(genDiff(getPathToFixture('nestedBefore.json'), getPathToFixture('nestedAfter.json')))
+      .toBe(expected);
+  });
+
+  it('#YAML', () => {
+    expect(genDiff(getPathToFixture('nestedBefore.yaml'), getPathToFixture('nestedAfter.yaml')))
+      .toBe(expected);
+  });
+
+  it('#INI', () => {
+    expect(genDiff(getPathToFixture('nestedBefore.ini'), getPathToFixture('nestedAfter.ini')))
+      .toBe(expected);
   });
 });
